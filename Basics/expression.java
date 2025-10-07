4ecda5052bc3c9f583bb01d118b7c7c07b567e4f
Ayush04H <ayush050419@gmail.com>
2025-10-08 00:21:32 +0530

    Basic Expressions

---
+++ Basics/expression.java
@@ -0,0 +1,20 @@
+public class expression{
+    public static void main(String[] args) {
+        // Operation = Operands & Operator
+        // Operands = Values , Variables , Numbers , Quantity
+        // Operators = + - / * % 
+
+        int a = 10;
+        int b = a / 2;
+        int c = b % 2;
+        int d = a + 2;
+        int e = a - 2;
+        int f = a * 2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
